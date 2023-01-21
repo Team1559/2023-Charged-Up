f2dafdcc5f26c3d53534814f7fff88eca979a295
Software <1559DevilTech@gmail.com>
2023-01-21 08:05:47 -0800

    Implement pure vision position tracking

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -1,15 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import java.io.IOException;
 import java.util.Objects;
 import java.util.Optional;
@@ -19,50 +9,68 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
 
-    private PhotonCamera camera;
+    private PhotonCamera        camera;
     private AprilTagFieldLayout aprilTagFieldLayout;
     private PhotonPoseEstimator photonPoseEstimator;
-    /*Constructor method for Vision class */
+    private Pose3d lastPose;
+
+    /* Constructor method for Vision class */
     public Vision() {
         camera = new PhotonCamera("OV5647");
         try {
-            aprilTagFieldLayout = new AprilTagFieldLayout(AprilTagFields.k2023ChargedUp.m_resourceFile);
+            aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
         } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            throw new IllegalStateException(e);
         }
         double cameraXOffset = Units.inchesToMeters(16);
         double cameraZOffset = Units.inchesToMeters(5.5);
-        Transform3d robotToCam = new Transform3d(new Translation3d(cameraXOffset, 0.0, cameraZOffset), new Rotation3d(0,0,0)); 
-        photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.LOWEST_AMBIGUITY, camera, robotToCam);
-
+        Transform3d robotToCam = new Transform3d(
+                new Translation3d(cameraXOffset, 0.0, cameraZOffset),
+                new Rotation3d(0, 0, 0));
+        photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout,
+                PoseStrategy.LOWEST_AMBIGUITY, camera, robotToCam);
+        // lastPose = new Pose3d(new Translation3d(), new Rotation3d());
     }
-    
-    
+
     @Override
-    public void periodic(){
+    public void periodic() {
+        // photonPoseEstimator.setReferencePose(lastPose);
         Optional<EstimatedRobotPose> result = photonPoseEstimator.update();
         if (result.isPresent()) {
             EstimatedRobotPose pose = result.get();
-            Pose2d pose2d = pose.estimatedPose.toPose2d();
-            SmartDashboard.putNumber("pose X", pose2d.getX());
-            SmartDashboard.putNumber("pose Y", pose2d.getY());
-            SmartDashboard.putNumber("pose Heading", pose2d.getRotation().getDegrees());
+            lastPose = new Pose3d(pose.estimatedPose.getTranslation(), pose.estimatedPose.getRotation());
+            SmartDashboard.putNumber("pose X", lastPose.getX());
+            SmartDashboard.putNumber("pose Y", lastPose.getY());
+            SmartDashboard.putNumber("pose Z", lastPose.getZ());
+            SmartDashboard.putNumber("pose Heading", lastPose.getRotation()
+                                                           .getAngle() / Math.PI * 180);
             SmartDashboard.putNumber("pose Timestamp", pose.timestampSeconds);
-             //new Pair<Pose2d, Double>(result.get().getFirst().toPose2d(), currentTime - result.get().getSecond());
+            // new Pair<Pose2d, Double>(result.get().getFirst().toPose2d(),
+            // currentTime - result.get().getSecond());
         }
         SmartDashboard.putString("Result str", Objects.toString(result));
         // Query the latest result from PhotonVision
         // PhotonPipelineResult result = camera.getLatestResult();
         // SmartDashboard.putBoolean("Has Target", result.hasTargets());
-        // SmartDashboard.putNumber("Result Timestamp", result.getTimestampSeconds());
+        // SmartDashboard.putNumber("Result Timestamp",
+        // result.getTimestampSeconds());
         // SmartDashboard.putNumber("Time", System.currentTimeMillis());
-        
+
         // if (!result.hasTargets()){
-        //     return;
+        // return;
         // }
         // PhotonTrackedTarget bestTarget = result.getBestTarget();
         // SmartDashboard.putNumber("Target ID", bestTarget.getFiducialId());
