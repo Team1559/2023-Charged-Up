bfff44894fd4bdd5cd9c1248849c8ba2a6146cee
Software <1559DevilTech@gmail.com>
2023-02-04 15:43:41 -0500

    Changed to servo Instances
    Commented out motor parts of methods

--- src/main/java/frc/robot/commands/TeleopWristAngleCommand.java
+++ src/main/java/frc/robot/commands/TeleopWristAngleCommand.java
@@ -1,8 +1,9 @@
 package frc.robot.commands;
 
-import static frc.robot.Constants.Grabber.MAXIMUN_WRIST_ANGLE;
-import static frc.robot.Constants.Grabber.MINIMUM_WRIST_ANGLE;
-import static frc.robot.Constants.Grabber.TELEOP_ANGULAR_VELOCITY_PER_CYCLE;
+// import static frc.robot.Constants.Grabber.MAXIMUN_WRIST_ANGLE;
+// import static frc.robot.Constants.Grabber.MINIMUM_WRIST_ANGLE;
+// import static frc.robot.Constants.Grabber.TELEOP_ANGULAR_VELOCITY_PER_CYCLE;
+import static frc.robot.Constants.Grabber.ZERO_ANGLE;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.lib.DTXboxController;
@@ -25,21 +26,30 @@ public class TeleopWristAngleCommand extends CommandBase {
         // If negative, rotate the wrist left by a set amount.
         // If positive, rotate the wrist right.
         double x = controller.getRightStickX();
-        double angle = wrist.getAngle();
+        //double angle = wrist.getAngle();
 
-        if (Math.abs(x) > deadband) {
-            if (x < 0) {
-                angle -= TELEOP_ANGULAR_VELOCITY_PER_CYCLE;
-            } else {
-                angle += TELEOP_ANGULAR_VELOCITY_PER_CYCLE;
+        if(Math.abs(x)>deadband){
+            if(x<(ZERO_ANGLE-deadband)||x>(ZERO_ANGLE+deadband)){
+                wrist.resetWrist();
             }
-            if (angle > MAXIMUN_WRIST_ANGLE) {
-                angle = 90;
-            }
-            if (angle < -MINIMUM_WRIST_ANGLE) {
-                angle = -90;
-            }
-            wrist.setAngle(angle);
         }
+        else{
+            wrist.setAngle(x);
+        }
+
+        // if (Math.abs(x) > deadband) {
+        //     if (x < 0) {
+        //         angle -= TELEOP_ANGULAR_VELOCITY_PER_CYCLE;
+        //     } else {
+        //         angle += TELEOP_ANGULAR_VELOCITY_PER_CYCLE;
+        //     }
+        //     if (angle > MAXIMUN_WRIST_ANGLE) {
+        //         angle = 90;
+        //     }
+        //     if (angle < -MINIMUM_WRIST_ANGLE) {
+        //         angle = -90;
+        //     }
+        //     wrist.setAngle(angle);
+        // }
     }
 }
--- src/main/java/frc/robot/subsystems/grabber/GrabberWrist.java
+++ src/main/java/frc/robot/subsystems/grabber/GrabberWrist.java
@@ -1,49 +1,50 @@
 package frc.robot.subsystems.grabber;
 
-import static frc.robot.Constants.Grabber.MAX_ANGULAR_VELOCITY;
+//import static frc.robot.Constants.Grabber.MAX_ANGULAR_VELOCITY;
 import static frc.robot.Constants.Grabber.SERVO_RANGE;
 import static frc.robot.Constants.Grabber.ZERO_ANGLE;
-//import static frc.robot.Constants.Wiring.WRIST_SERVO_PORT; IF SERVO
-import static frc.robot.Constants.Wiring.WRIST_CANCODER_ID;
-import static frc.robot.Constants.Wiring.WRIST_MOTOR_PORT;
+import static frc.robot.Constants.Wiring.WRIST_SERVO_PORT;// when SERVO
+// import static frc.robot.Constants.Wiring.WRIST_CANCODER_ID;
+// import static frc.robot.Constants.Wiring.WRIST_MOTOR_PORT;
 
-//import edu.wpi.first.wpilibj.Servo; IF SERVO
+import edu.wpi.first.wpilibj.Servo;// wheN SERVO
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.sensors.AbsoluteSensorRange;
-import com.ctre.phoenix.sensors.CANCoder;
+// import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+// import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+// import com.ctre.phoenix.sensors.CANCoder;
 
-import edu.wpi.first.math.controller.PIDController;
+//import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+//import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class GrabberWrist extends SubsystemBase {
-    //IF WE DO SERVO private Servo wristServo; 
-    private WPI_TalonSRX wristMotor;
-    private CANCoder canCoder;
-    private PIDController pidController;
+    private Servo wristServo; 
+    //private WPI_TalonSRX wristMotor;
+    //private CANCoder canCoder;
+    //private PIDController pidController;
     public GrabberWrist() {
-        //wristServo = new Servo(WRIST_SERVO_PORT); if servo
-        wristMotor = new WPI_TalonSRX(WRIST_MOTOR_PORT);
-        canCoder = new  CANCoder(WRIST_CANCODER_ID);
-        canCoder.configAbsoluteSensorRange(AbsoluteSensorRange.Signed_PlusMinus180);
-        pidController = new PIDController(0.1, 0.0, 0.0);
+        wristServo = new Servo(WRIST_SERVO_PORT);// if servo
+        //wristMotor = new WPI_TalonSRX(WRIST_MOTOR_PORT);
+        //canCoder = new  CANCoder(WRIST_CANCODER_ID);
+        //canCoder.configAbsoluteSensorRange(AbsoluteSensorRange.Signed_PlusMinus180);
+        //pidController = new PIDController(0.1, 0.0, 0.0);
     }
     
 
     public void setAngle(double grabberAngle) {// Verify selected servo is 0-180
-        /**
-         *         wristServo.setAngle(grabberAngle + ZERO_ANGLE); // if not use
-         * wristServo.set()
-         * instead (0.0 to 1.0)
-         */
-        double currentPosition = canCoder.getAbsolutePosition(); // find current position
-        double pidValue = pidController.calculate(grabberAngle, currentPosition);
-        wristMotor.set(pidValue);
+        
+                  wristServo.setAngle(grabberAngle + ZERO_ANGLE); //
+         // * if not use
+         //* wristServo.set()
+        // * instead (0.0 to 1.0)
+         //*/
+        // double currentPosition = canCoder.getAbsolutePosition(); // find current position
+        // double pidValue = pidController.calculate(grabberAngle, currentPosition);
+        // wristMotor.set(pidValue);
     
         /** pass current position to PID controller and get value from PID representing amount of motor power
         *set motor to that value
@@ -51,32 +52,28 @@ public class GrabberWrist extends SubsystemBase {
     }
 
     public double getAngle() {
-        //return wristServo.getAngle(); IF SERVO
-        return canCoder.getAbsolutePosition();
+        return wristServo.getAngle(); 
+        //return canCoder.getAbsolutePosition();
         //returns from -180 to +180 ^
     }
 
     public Command setWristAngleCommand(double angle) {
         return Commands.sequence(
-                new InstantCommand(() -> setAngle(angle), this),
-                new WaitCommand(
-                        Math.abs(angle - getAngle()) / MAX_ANGULAR_VELOCITY));
+                new InstantCommand(() -> setAngle(angle), this));
+                // new WaitCommand(
+                //         Math.abs(angle - getAngle()) / MAX_ANGULAR_VELOCITY));
     }
 
     public void resetWrist() {
-        //wristServo.setAngle(ZERO_ANGLE); IF SERVO
+        wristServo.setAngle(ZERO_ANGLE);
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("Grabber Angle",
-                wristMotor.get() * SERVO_RANGE - ZERO_ANGLE);
-        SmartDashboard.putNumber("Grabber Servo Angle", wristMotor.get());
-    }
-    /**  IF SERVO
-     *  wristServo.get() * SERVO_RANGE - ZERO_ANGLE);
-        SmartDashboard.putNumber("Grabber Servo Angle", wristServo.get());
-     */
+        //SmartDashboard.putNumber("Grabber Angle",wristMotor.get() * SERVO_RANGE - ZERO_ANGLE);
+        SmartDashboard.putNumber("Grabber Angle",wristServo.get() * SERVO_RANGE - ZERO_ANGLE);
+    SmartDashboard.putNumber("Grabber Servo Angle", wristServo.getAngle());
+}
     // private double calculateServoAngle(double targetAngle){
     // return targetAngle+ZERO_ANGLE;
     // }
