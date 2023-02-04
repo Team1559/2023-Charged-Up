bcbf9ab1d36dbc32e5c6aee0075454e54fccc2b7
JohnBurdick1 <alloysteel1023@gmail.com>
2023-02-04 14:51:51 -0500

    test code for ArmBase movement

--- src/main/java/frc/robot/subsystems/arm/FullArmCommands.java
+++ src/main/java/frc/robot/subsystems/arm/FullArmCommands.java
@@ -1,9 +1,26 @@
 package frc.robot.subsystems.arm;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class FullArmCommands extends CommandBase{
+import frc.robot.subsystems.arm.ArmBase;
+import frc.robot.subsystems.arm.ArmElbow;
+import frc.robot.subsystems.arm.ArmWrist;
+
+public class FullArmCommands{
     private ArmBase base;
     private ArmElbow elbow;
     private ArmWrist wrist;
+
+    public FullArmCommands(ArmBase base, ArmElbow elbow, ArmWrist wrist){
+        this.base = base;
+        this.elbow = elbow;
+        this.wrist = wrist;
+    }
+    public ParallelCommandGroup armToPos1 = new ParallelCommandGroup(
+        new Command setBaseAngleCommandPos(0, base),
+        new Command setElbowAngleCommandPos(0, elbow),
+        new Command setWristAngleCommandPos(0, wrist)
+    );
 }
