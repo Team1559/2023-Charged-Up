4c9012eddc387c04713e988f6b0ad93b0b19f1ab
Michael Marchetti <mike@themarchettis.com>
2023-02-10 18:55:03 -0500

    factor out a common ArmSegment base class

--- src/main/java/frc/robot/subsystems/arm/ArmBase.java
+++ src/main/java/frc/robot/subsystems/arm/ArmBase.java
@@ -1,117 +1,18 @@
 package frc.robot.subsystems.arm;
 
-import com.ctre.phoenix.motorcontrol.DemandType;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-
-import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import static frc.robot.Constants.Wiring.ARM_MOTOR_ID_BASE;
-import static frc.robot.Constants.Arm.INV_GEAR_RATIO_BASE;
-import static frc.robot.Constants.FALCON_TICKS_PER_REV;
 import static frc.robot.Constants.Arm.*;
+import static frc.robot.Constants.Wiring.ARM_MOTOR_ID_BASE;
+import static frc.robot.Constants.Wiring.BASE_CANCODER_ID;
+import edu.wpi.first.math.controller.ArmFeedforward;
 
-public class ArmBase extends SubsystemBase {
+public class ArmBase extends ArmSegment {
+    private static final double[] basePos = { 0, 10, 20, 30, 40, 50, 60, 70, 80,
+            90 };
 
-    private final TalonFX             baseMotor;
-    //private final CANCoder canCoder;
-    private final ArmFeedforward      feedforward;
-    private final double[]            basePos = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90};
-    private int degrees;
-    
     public ArmBase() {
-        //canCoder = new CANCoder(BASE_CANCODER_ID);
-        baseMotor = new TalonFX(ARM_MOTOR_ID_BASE);
-        baseMotor.configFactoryDefault();
-        baseMotor.enableVoltageCompensation(true);
-        SupplyCurrentLimitConfiguration limit = new SupplyCurrentLimitConfiguration(true, 40, 60, 0.5);
-        baseMotor.configSupplyCurrentLimit(limit);
-        baseMotor.config_kP(0, kP_BASE);
-        baseMotor.config_kI(0, kI_BASE);
-        baseMotor.config_kD(0, kD_BASE);
-        baseMotor.config_IntegralZone(0, angleToTick(kIZ_BASE));
-        baseMotor.setSelectedSensorPosition(angleToTick(90));
-        // baseMotor.setInverted(true);
-        feedforward = new ArmFeedforward(kS_BASE, kG_BASE, kV_BASE, kA_BASE);
-    }
-    public void resetEncoderForTesting(double angle) {
-        baseMotor.setSelectedSensorPosition(angleToTick(angle));
-    }
-    public double getAngle() {
-        return tickToAngle(baseMotor.getSelectedSensorPosition());
-    }
-
-    public static double angleToTick(double angle) {
-        // Approx 524 ticks per degree
-        double revolutionsOfArm = angle / 360.0;
-        double motorRevolutions = revolutionsOfArm * INV_GEAR_RATIO_BASE;
-        double angleTicks = motorRevolutions * FALCON_TICKS_PER_REV;
-        return angleTicks;
-    }
-    public static double tickToAngle(double ticks){
-        double motorRevolutions = ticks / FALCON_TICKS_PER_REV;
-        double revolutionsOfArm = motorRevolutions/INV_GEAR_RATIO_BASE;
-        double angle = revolutionsOfArm * 360;
-        return angle;
-    }
-
-    public void setAngle(double angle) {
-        double FF = feedforward.calculate(angle, 0, 0) / 12.0;
-        SmartDashboard.putNumber("base feedforward", FF);
-        baseMotor.set(TalonFXControlMode.Position, angleToTick(angle),
-                DemandType.ArbitraryFeedForward, FF);
-    }
-
-    public Command setBaseAngleCommandPos(int angleIndex) {
-        double angle = basePos[angleIndex];
-        return new FunctionalCommand(() -> {setAngle(angle);}, () -> {}, (a) -> {}, () -> {return IsArmBaseAtPosition(angle);}, this);
-    }
-
-    public int degreeSetUp(){
-        degrees += 10;
-        if (degrees > 90){
-            degrees = 90;
-        }
-        return degrees;
-    }
-    public int degreeSetDown(){
-        degrees -= 10;
-        if (degrees < 0){
-            degrees = 0;
-        }
-        return degrees;
-    }
-    public Command degreesUp(){
-        return Commands.sequence(
-            new InstantCommand(() -> degreeSetUp(), this),
-            new InstantCommand(() -> setAngle(degrees), this)
-        );
-    }
-    public Command degreesDown(){
-        return Commands.sequence(
-            new InstantCommand(() -> degreeSetDown(), this),
-            new InstantCommand(() -> setAngle(degrees), this)
-        );
-    }
-    public boolean IsArmBaseAtPosition(double angle){
-        double angleError = Math.abs(angle - getAngle());
-        if (angleError < MAXIMUM_ANGLE_ERROR){
-            return true;
-        }
-        return false;
-    }
-    @Override
-    public void periodic() {
-        SmartDashboard.putNumber("Base angle: ", getAngle());
-        SmartDashboard.putNumber("Base error: ", baseMotor.getClosedLoopError());
-        SmartDashboard.putNumber("Base current: ", baseMotor.getStatorCurrent());
-        SmartDashboard.putNumber("Base temperature: ", baseMotor.getTemperature());
+        super("Base", ARM_MOTOR_ID_BASE, BASE_CANCODER_ID, kP_BASE, kI_BASE,
+                kD_BASE, kIZ_BASE, INV_GEAR_RATIO_BASE,
+                new ArmFeedforward(kS_BASE, kG_BASE, kV_BASE, kA_BASE),
+                basePos);
     }
 }
--- src/main/java/frc/robot/subsystems/arm/ArmElbow.java
+++ src/main/java/frc/robot/subsystems/arm/ArmElbow.java
@@ -1,110 +1,17 @@
 package frc.robot.subsystems.arm;
 
-import com.ctre.phoenix.motorcontrol.DemandType;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-
-import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import static frc.robot.Constants.Wiring.ARM_MOTOR_ID_ELBOW;
-import static frc.robot.Constants.Arm.INV_GEAR_RATIO_BASE;
-import static frc.robot.Constants.FALCON_TICKS_PER_REV;
 import static frc.robot.Constants.Arm.*;
+import static frc.robot.Constants.Wiring.ARM_MOTOR_ID_ELBOW;
+import static frc.robot.Constants.Wiring.ELBOW_CANCODER_ID;
+import edu.wpi.first.math.controller.ArmFeedforward;
 
-public class ArmElbow extends SubsystemBase {
-    //private final CANCoder      canCoder;
-    private final TalonFX       elbowMotor;
-    private ArmFeedforward      feedforward;
-    private final double[]      elbowPos = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    private int degrees;
+public class ArmElbow extends ArmSegment {
+    private static final double[] elbowPos = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
     public ArmElbow() {
-        //canCoder = new CANCoder(ELBOW_CANCODER_ID);
-        elbowMotor = new TalonFX(ARM_MOTOR_ID_ELBOW);
-        elbowMotor.configFactoryDefault();
-        elbowMotor.enableVoltageCompensation(true);
-        SupplyCurrentLimitConfiguration limit = new SupplyCurrentLimitConfiguration(
-                true, 40, 60, 0.5);
-        elbowMotor.configSupplyCurrentLimit(limit);
-        elbowMotor.config_kP(0, kP_ELBOW);
-        elbowMotor.config_kI(0, kI_ELBOW);
-        elbowMotor.config_kD(0, kD_ELBOW);
-        feedforward = new ArmFeedforward(kS_ELBOW, kG_ELBOW, kV_ELBOW, kA_ELBOW);
-    }
-
-    public double getAngle() {
-        return elbowMotor.getSelectedSensorPosition();
-    }
-
-    public static double angleToTick(double angle) {
-        double revolutionsOfArm = angle / 360.0;
-        double motorRevolutions = revolutionsOfArm * INV_GEAR_RATIO_BASE;
-        double angleTicks = motorRevolutions * FALCON_TICKS_PER_REV;
-        return angleTicks;
-    }
-    public static double tickToAngle(double ticks){
-        double motorRevolutions = ticks / FALCON_TICKS_PER_REV;
-        double revolutionsOfArm = motorRevolutions/INV_GEAR_RATIO_BASE;
-        double angle = revolutionsOfArm * 360;
-        return angle;
-    }
-
-    public void setAngle(double angle) {
-        double FF = feedforward.calculate(angle, 0, 0);
-        elbowMotor.set(TalonFXControlMode.Position, angleToTick(angle),
-                DemandType.ArbitraryFeedForward, FF);
-    }
-
-    public int degreeSetUp(){
-        degrees += 10;
-        if (degrees > 90){
-            degrees = 90;
-        }
-        return degrees;
-    }
-    public int degreeSetDown(){
-        degrees -= 10;
-        if (degrees < 0){
-            degrees = 0;
-        }
-        return degrees;
-    }
-    public Command degreesUp(){
-        return Commands.sequence(
-            new InstantCommand(() -> degreeSetUp(), this),
-            new InstantCommand(() -> setAngle(degrees), this)
-        );
-    }
-    public Command degreesDown(){
-        return Commands.sequence(
-            new InstantCommand(() -> degreeSetDown(), this),
-            new InstantCommand(() -> setAngle(degrees), this)
-        );
-    }
-
-    public Command setElbowAngleCommandPos(int angleIndex) {
-        double angle = elbowPos[angleIndex];
-        return new FunctionalCommand(() -> {setAngle(angle);}, () -> {}, (a) -> {}, () -> {return isArmElbowAtPosition(angle);}, this);
-    }
-
-    public boolean isArmElbowAtPosition(double angle){
-        double angleError = Math.abs(angle - getAngle());
-        if (angleError < MAXIMUM_ANGLE_ERROR){
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public void periodic() {
-        SmartDashboard.putNumber("Elbow CANCoder reading (in deg): ",
-                getAngle());
+        super("Elbow", ARM_MOTOR_ID_ELBOW, ELBOW_CANCODER_ID, kP_ELBOW,
+                kI_ELBOW, kD_ELBOW, kIZ_ELBOW, INV_ELBOW_GEAR_RATIO,
+                new ArmFeedforward(kS_ELBOW, kG_ELBOW, kV_ELBOW, kA_ELBOW),
+                elbowPos);
     }
 }
