74db5398ea5445ea21f7383a5157ae847093b48b
JohnBurdick1 <alloysteel1023@gmail.com>
2023-03-07 18:51:13 -0500

    minor changes

--- src/main/java/frc/robot/subsystems/arm/ArmSegment.java
+++ src/main/java/frc/robot/subsystems/arm/ArmSegment.java
@@ -19,8 +19,6 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public abstract class ArmSegment extends SubsystemBase {
